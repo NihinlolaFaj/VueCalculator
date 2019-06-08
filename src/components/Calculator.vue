e81a8afb4974e068d4d88f714ea1e4a1a4d6975e
Nihinlolamiwa Fajemilehin <nihinfaj@gmail.com>
2019-06-08 19:12:29 +0100

    Comments added

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,15 +1,10 @@
 <template>
   <div>
-    <div>
-      <b-alert v-model="showDismissibleAlert" variant="danger" dismissible>
-      Dismissible Alert!
-    </b-alert>
-    </div>
     <div class="calculator">
       <div class="display">{{current || '0'}}</div>
       <div @click="clear" class="btn">C</div>
       <div class="btn">√</div>
-      <div @click="percent" class="btn">%</div>
+      <div class="btn">π</div>
       <div @click="divide" class="btn operator">÷</div>
       <div @click="append('7')" class="btn">7</div>
       <div @click="append('8')" class="btn">8</div>
@@ -23,9 +18,8 @@
       <div @click="append('2')" class="btn">2</div>
       <div @click="append('3')" class="btn">3</div>
       <div @click="add" class="btn operator">+</div>
-      <div @click="append('0')" class="btn">0</div>
+      <div @click="append('0')" class="btn zero">0</div>
       <div @click="dot" class="btn">.</div>
-      <div class="btn">π</div>
       <div @click="equal" class="btn operator">=</div>
     </div>
   </div>
@@ -35,60 +29,72 @@
 export default {
   data() {
     return {
-      previous: null,
+      previous: "",
       current: "",
       operator: null,
       operatorValue: null,
       operatorClicked: false,
-      showDismissibleAlert: false,
     };
   },
   methods: {
+    // Clear Function that clears the Display Field
     clear() {
       this.current = "";
     },
-    percent() {
-      this.current = `${parseFloat(this.current) / 100}`;
-    },
+    // Append function used to append values on the display field
     append(number) {
-      // if() {
-
-      // }
-      if (this.operatorClicked) {
+      if(this.current.length < 9) {
+        if (this.operatorClicked) {
         this.current = "";
         this.operatorClicked = false;
       }
       this.current = `${this.current}${number}`;
+      console.log("Total is " + this.current.length);
+      }
+      else {
+        console.log("I can't add no more!");
+      }
+      
     },
+    // dot function used to add a dot operand to the display field
     dot() {
       if (this.current.indexOf(".") === -1) {
         this.append(".");
       }
     },
+    // setPreviousData function used to store current display value into a previous variable
+    // so at carry out calculation
     setPreviousData() {
       this.previous = this.current;
       this.operatorClicked = true;
     },
+    // divide function that stores the value of the divide operator
     divide() {
       this.operatorValue = "/";
       this.operator = (a, b) => a / b;
       this.setPreviousData();
     },
+    // times function that stores the value of the times operator
     times() {
       this.operatorValue = "*";
       this.operator = (a, b) => a * b;
       this.setPreviousData();
     },
+    // minus function that stores the value of the minus operator
     minus() {
       this.operatorValue = "-";
       this.operator = (a, b) => a - b;
       this.setPreviousData();
     },
+    // add function that stores the value of the add operator
     add() {
       this.operatorValue = "+";
       this.operator = (a, b) => a + b;
       this.setPreviousData();
     },
+    // equal function that takes combines the arithemetic values and operator to be calculated, 
+    // calls the Java REST API to perform calculation, receives the result of the calculation,
+    // and displays the result of the calculation in the display field
     equal() {
       console.log(
         this.previous + " " + this.operatorValue + " " + this.current
@@ -103,7 +109,7 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- The customised stylesheet for the calculator app -->
 <style scoped>
 .calculator {
   width: 400px;
@@ -126,5 +132,8 @@ export default {
 .operator {
   background-color: orange;
   color: white;
+}
+.zero {
+  grid-column: 1 / 3;
 }
 </style>
